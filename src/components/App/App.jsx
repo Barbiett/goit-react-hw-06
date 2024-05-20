81248d178df3b247e14090289e7fa7081e519afa
Barbiet <bogdanvinokurov2000@gmail.com>
2024-05-20 14:51:40 +0300

    7

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,25 +4,11 @@ import ContactList from "../ContactList/ContactList";
 
 import css from "./App.module.css";
 
-import * as Yup from "yup";
-const UserSchema = Yup.object().shape({
-  username: Yup.string()
-    .trim()
-    .min(3, "Too Short")
-    .max(50, "Max 50 letters!")
-    .required("Is required"),
-  usernumber: Yup.string()
-    .min(3, "Min 3 numbers!")
-    .matches(/^\d{1,50}$/, "Max 50 numbers!")
-    .max(50, "Max 50 numbers!")
-    .required("Is required!"),
-});
-
 export default function App() {
   return (
     <div className={css.container}>
       <h1 className={css.header}>Phonebook</h1>
-      <ContactForm UserSchema={UserSchema} />
+      <ContactForm />
       <SearchBox />
       <ContactList />
     </div>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,8 +4,21 @@ import { nanoid } from "nanoid";
 import { addContact } from "../../redux/contactsSlice";
 import { useDispatch } from "react-redux";
 import css from "./ContactForm.module.css";
+import * as Yup from "yup";
+const UserSchema = Yup.object().shape({
+  username: Yup.string()
+    .trim()
+    .min(3, "Too Short")
+    .max(50, "Max 50 letters!")
+    .required("Is required"),
+  usernumber: Yup.string()
+    .min(3, "Min 3 numbers!")
+    .matches(/^\d{1,50}$/, "Max 50 numbers!")
+    .max(50, "Max 50 numbers!")
+    .required("Is required!"),
+});
 
-export default function ContactForm({ UserSchema }) {
+export default function ContactForm() {
   const dispatch = useDispatch();
   const userNameId = useId();
   const userNamumberId = useId();
